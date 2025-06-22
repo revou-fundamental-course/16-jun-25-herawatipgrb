81b63db155538e0ea85f75ec4468dd384e40fba7
herawatipgrb <herawatypgrb@gmail.com>
2025-06-22 21:13:56 +0700

    upload project kalkulator segitiga

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+
 function hitungLuas() {
     const alas = parseFloat(document.getElementById("alas").value);
     const tinggi = parseFloat(document.getElementById("tinggi").value);
 
     if (isNaN(alas) || isNaN(tinggi)) {
-        alert("Masukkan nilai alas dan tinggi yang valid!");
+        alert("Masukkan angka yang valid untuk alas dan tinggi.");
         return;
     }
 
     const luas = 0.5 * alas * tinggi;
-
     document.getElementById("hasilLuas").innerHTML = 
-        `L = 1/2 x a x t<br>L = 1/2 x ${alas} x ${tinggi}<br>L = ${luas}`;
+        "L = 1/2 x a x t<br>" +
+        "L = 1/2 x " + alas + " x " + tinggi + "<br>" +
+        "L = " + luas;
 }
